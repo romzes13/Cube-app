60ca45d3a36e729b1ca612ca83076e81ca319d4d
romzes13 <romzes13@users.noreply.github.com>
2016-12-21 01:28:50 -0500

    Add files via upload

---
+++ test.java
@@ -0,0 +1,19 @@
+package com.java.cube;
+
+import java.io.IOException;
+
+public class test {
+	
+	public static void main(String[] args) throws IOException 
+	  { 
+		System.out.println("New branch added!");
+		
+		
+		
+		System.out.println("Hello!");
+		
+		
+	  }
+	
+
+}
